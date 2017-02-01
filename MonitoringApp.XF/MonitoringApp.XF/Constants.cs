af18a4c5298f7706d5286ccf419e6297cfbe46f4
Guillaume Teirlinck <guillaume.teirlinck@gsgfintech.com>
2017-02-01 17:08:59 +0800

    Dependency upgrade

--- MonitoringApp.XF/MonitoringApp.XF/Constants.cs
+++ MonitoringApp.XF/MonitoringApp.XF/Constants.cs
@@ -2,6 +2,6 @@
 {
     public static class Constants
     {
-        public static string ApplicationURL = @"https://stratedgeme-monitor-dev.azurewebsites.net";
+        public static string ApplicationURL = @"https://stratedgeme-monitor-mobile-backend.azurewebsites.net";
     }
 }
